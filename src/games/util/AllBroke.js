89a60a517efb806fbb75e86eaea26d3d3f1fbed8
Energyzor <114320299+Energyzor@users.noreply.github.com>
2023-03-18 12:47:01 +0000

    Create AllBroke.js

---
+++ src/games/util/AllBroke.js
@@ -0,0 +1,30 @@
+import data from "../../data";
+import ResetBall from "./ResetBall";
+import Brick from "../Brick";
+
+export default function AllBroken(bricks, player, canvas, balls, ctx) {
+    let { brickObj, paddleProps } = data;
+    let total = 0;
+    for (let i = 0; i < bricks.length; i++) {
+        if (bricks[i].broke === true) {
+            total++;
+        }
+    }
+    if (total === bricks.length) {
+        if (player.level === 10) {
+            alert("You Win! Press OK to restart");
+            player.level = 1;
+            player.lives = 5;
+            player.score = 0;
+            ResetBall(balls, canvas, paddleProps, brickObj);
+            bricks.length = 0;
+            brickObj.y = 10;
+            Brick(player.level, bricks, canvas, brickObj, ctx);
+        } else {
+            player.level++;
+            ResetBall(balls, canvas, paddleProps, brickObj);
+            brickObj.y = 10;
+            Brick(player.level, bricks, canvas, brickObj, ctx);
+        }
+    }
+}
